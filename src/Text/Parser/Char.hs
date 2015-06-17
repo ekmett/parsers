337779e7054423b2e1c88217cf1308cdc110d63a
Julien Cretel <j.cretel@umail.ucc.ie>
2015-06-17 16:05:59 +0100

    Fix some typos and missing or out-of-place words

--- src/Text/Parser/Char.hs
+++ src/Text/Parser/Char.hs
@@ -81,7 +81,7 @@ oneOf xs = oneOfSet (CharSet.fromList xs)
 {-# ANN oneOf "HLint: ignore Use String" #-}
 
 -- | As the dual of 'oneOf', @noneOf cs@ succeeds if the current
--- character /not/ in the supplied list of characters @cs@. Returns the
+-- character is /not/ in the supplied list of characters @cs@. Returns the
 -- parsed character.
 --
 -- >  consonant = noneOf "aeiou"
@@ -101,7 +101,7 @@ oneOfSet (CharSet False _ is) = satisfy (\c -> not (IntSet.member (fromEnum c) i
 {-# INLINE oneOfSet #-}
 
 -- | As the dual of 'oneOf', @noneOf cs@ succeeds if the current
--- character /not/ in the supplied list of characters @cs@. Returns the
+-- character is /not/ in the supplied list of characters @cs@. Returns the
 -- parsed character.
 --
 -- >  consonant = noneOf "aeiou"

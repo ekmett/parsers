59aa1d70e10bef248847343edac7bb802ad3a57a
Mario Blažević <blamario@protonmail.com>
2020-04-25 09:17:36 -0400

    Removed a superflous language pragma

--- src/Text/Parser/Input.hs
+++ src/Text/Parser/Input.hs
@@ -3,7 +3,6 @@
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE TypeFamilies #-}
-{-# LANGUAGE TypeSynonymInstances #-}
 
 -----------------------------------------------------------------------------
 -- |
